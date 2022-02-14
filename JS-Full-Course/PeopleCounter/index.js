20c3ddd1fd3430f0149b3c8f9aa90e26081ea368
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2022-02-14 13:58:09 +0100

    Add files via upload

---
+++ JS-Full-Course/PeopleCounter/index.js
@@ -0,0 +1,23 @@
+let amountOfPeople = 0;
+let peopleCounterHtmlElement = document.getElementById("count-el")
+let storedPeople = 'Saved entries: '
+let peopleStoredHtmlElement = document.getElementById("storedPeople")
+
+const showAmountOfPeople = (amountOfPeople) => {
+    peopleCounterHtmlElement.innerText = amountOfPeople;
+}
+
+const peopleCounter = () => {
+    amountOfPeople += 1;
+    showAmountOfPeople(amountOfPeople)
+}
+
+const saveAmountOfPeople = () => {
+    storedPeople += amountOfPeople + ' - '
+    peopleStoredHtmlElement.innerText = storedPeople
+}
+
+const resetPeopleCounter = () => {
+    amountOfPeople = 0;
+    showAmountOfPeople(amountOfPeople)
+}
