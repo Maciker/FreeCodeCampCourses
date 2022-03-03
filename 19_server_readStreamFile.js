49800da602e29e5be8e242af7448c2baa41bd221
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2022-03-03 11:41:52 +0100

    NodeJS Tutorial - Streams

---
+++ 19_server_readStreamFile.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf-8')
+    fileStream.on('open', () => {
+        fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+        res.destroy(err)
+    })
+}).listen(5000)
