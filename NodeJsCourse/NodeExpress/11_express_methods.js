db441a5355aac7dd96454e7470b374f86b3d7854
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2022-03-14 11:38:05 +0100

    Node Express - Express methods

--- NodeJsCourse/NodeExpress/11_express_methods.js
+++ NodeJsCourse/NodeExpress/11_express_methods.js
@@ -20,6 +20,44 @@ app.post('/api/people', (req, res) => {
     res.status(201).json({sucess: true, person: name})
 })
 
+app.post('/api/postman/people', (req, res) => {
+    const {name} = req.body
+    if(!name) {
+        res.status(400).json({success: false, msg:'Error!! Provide a name!!!'})
+    }
+    res.status(201).json({sucess: true, data: [...people, name]})
+})
+
+app.put('/api/people/:id', (req, res) => {
+    const {id} = req.params
+    const {name} = req.body
+    
+    const person = people.find((person) => person.id = Number(id))
+
+    if (!person) {
+        return res.status(400).json({success: false, msg:'Error!! No person with that id!!!'})
+    }
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+})
+
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if (!person) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `no person with id ${req.params.id}` })
+    }
+    const newPeople = people.filter(
+      (person) => person.id !== Number(req.params.id)
+    )
+    return res.status(200).json({ success: true, data: newPeople })
+  })
+
 app.post('/login', (req, res) => {
     const {name} = req.body
     if (name) {
