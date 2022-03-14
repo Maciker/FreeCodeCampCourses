83818e57c848c2554ab53eb9474db09cc3dfee8c
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2022-03-14 11:07:12 +0100

    Node Express - Express methods

---
+++ NodeJsCourse/NodeExpress/11_express_methods.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+let {people} = require('./data')
+
+app.use(express.static('./methods-public'))
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people})
+})
+
+app.post('/api/people', (req, res) => {
+    const {name} = req.body
+    if(!name) {
+        res.status(400).json({success: false, msg:'Error!! Provide a name!!!'})
+    }
+    res.status(201).json({sucess: true, person: name})
+})
+
+app.post('/login', (req, res) => {
+    const {name} = req.body
+    if (name) {
+        return res.status(200).send(`welcome ${name}`)
+    }
+    res.status(401).send(`<h1>Error: You must submit the name</h1`)
+})
+
+app.listen(5000, () => {
+    console.log('Server listening on port 5000...');
+})
